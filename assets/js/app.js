297ab4653f38052e678151c518d4809ac0452706
Miriam <miriammp1997@gmail.com>
2017-06-08 16:10:13 -0500

    ready the calculator

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
 
+$(_ => {
+    var screenVal = $('input:text');
+    $('input:button').on('click',function(){
+        var eachNumber      = $(this).val();
+        var currentScreen   = screenVal.val();
+        currentScreen      += eachNumber;
+        screenVal.val(currentScreen);
+        if ( $(this).val() == 'C') {
+            screenVal.val('');
+        }
+    });
+    $('button').click( (e) => {
+        e.preventDefault();
+        screenVal.val(eval(screenVal.val()));
+    })
+})
