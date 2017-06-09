9e8aa57b8431169804672d76888227a327c43a6d
Miriam <miriammp1997@gmail.com>
2017-06-08 19:34:18 -0500

    animated my button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,27 @@
 
-$(_ => {
-    var screenVal = $('input:text');
-    $('input:button').on('click',function(){
-        var eachNumber      = $(this).val();
-        var currentScreen   = screenVal.val();
-        currentScreen      += eachNumber;
-        screenVal.val(currentScreen);
-        if ( $(this).val() == 'C') {
-            screenVal.val('');
-        }
-    });
-    $('button').click( (e) => {
-        e.preventDefault();
-        screenVal.val(eval(screenVal.val()));
+    $(_ => {
+        var screenVal = $('input:text');
+        $('input:button').on('click', function (e) {
+            $(e.target).addClass('shadow-pulse');
+            $(e.target).animate({
+                opacity:0.2
+            }, 100, function(){
+                $(this).animate({
+                    opacity:1
+                });
+                $(e.target).removeClass('shadow-pulse');
+            })
+
+            var eachNumber      = $(this).val();
+            var currentScreen   = screenVal.val();
+            currentScreen      += eachNumber;
+            screenVal.val(currentScreen);
+            if ( $(this).val() == 'C') {
+                screenVal.val('');
+            }
+        });
+        $('button').click( (e) => {
+            e.preventDefault();
+            screenVal.val(eval(screenVal.val()));
+        })
     })
-})
